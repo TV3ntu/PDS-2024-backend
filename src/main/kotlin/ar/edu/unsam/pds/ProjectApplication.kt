abf2ccce0484173aff7e3d28c25a9f7d86e2965a
tventurini_meli <tomas.venturini@mercadolibre.com>
2024-04-14 22:07:14 -0300

    Cambia nombre de la clase projectApplication

--- src/main/kotlin/ar/edu/unsam/pds/ProjectApplication.kt
+++ src/main/kotlin/ar/edu/unsam/pds/ProjectApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class ProyectoApplication
+class ProjectApplication
 
 fun main(args: Array<String>) {
-    runApplication<ProyectoApplication>(*args)
+    runApplication<ProjectApplication>(*args)
 }
