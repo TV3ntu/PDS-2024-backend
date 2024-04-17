8290c8937404b5b5e813cdbb859abd012e3eb10c
ilrosaunsam <101224335+ilrosaunsam@users.noreply.github.com>
2024-04-17 19:50:55 -0300

    actualizo method models

--- src/main/kotlin/ar/edu/unsam/pds/models/Course.kt
+++ src/main/kotlin/ar/edu/unsam/pds/models/Course.kt
@@ -2,10 +2,15 @@ package ar.edu.unsam.pds.models
 import java.util.UUID
 import ar.edu.unsam.pds.repository.Element
 
-class Couse (
+class Course (
     val title: String,
     val description: String,
-    var category: String
+    var category: String,
+    var image: String
 ) : Element {
     val id: String = UUID.randomUUID().toString()
+
+    override fun findMe(value: String): Boolean {
+        TODO("Not yet implemented")
+    }
 }
--- src/main/kotlin/ar/edu/unsam/pds/models/Institution.kt
+++ src/main/kotlin/ar/edu/unsam/pds/models/Institution.kt
@@ -4,7 +4,11 @@ import ar.edu.unsam.pds.repository.Element
 
 class Institution (
     val name: String,
-    val category: String
+    val category: String,
+    var image: String
 ) : Element  {
     val id: String = UUID.randomUUID().toString()
+    override fun findMe(value: String): Boolean {
+        TODO("Not yet implemented")
+    }
 }
