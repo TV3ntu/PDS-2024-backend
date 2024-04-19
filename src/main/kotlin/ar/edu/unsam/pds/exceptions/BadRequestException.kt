23f97d066221b9b7c05f8875173752d36637f0d8
tventurini_meli <tomas.venturini@mercadolibre.com>
2024-04-19 20:37:12 -0300

    add basic test

--- src/main/kotlin/ar/edu/unsam/pds/exceptions/BadRequestException.kt
+++ src/main/kotlin/ar/edu/unsam/pds/exceptions/BadRequestException.kt
@@ -1,8 +1,8 @@
-package ar.edu.unsam.pds.exceptions
+    package ar.edu.unsam.pds.exceptions
 
-import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.ResponseStatus
+    import org.springframework.http.HttpStatus
+    import org.springframework.web.bind.annotation.ResponseStatus
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
-class BadRequestException(message: String) : RuntimeException(message){
-}
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    class BadRequestException(message: String) : RuntimeException(message){
+    }
