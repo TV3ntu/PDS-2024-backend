2150605dcbb57ffa7c2ed318a1ccf116d39f3c64
Rafael José <rafael_jose_calderon@hotmail.com>
2024-05-06 11:48:48 -0300

    solicitud_front_001_1

--- src/main/kotlin/ar/edu/unsam/pds/controllers/InstitutionController.kt
+++ src/main/kotlin/ar/edu/unsam/pds/controllers/InstitutionController.kt
@@ -2,6 +2,7 @@ package ar.edu.unsam.pds.controllers
 
 import ar.edu.unsam.pds.dto.InstitutionDto
 import ar.edu.unsam.pds.services.InstitutionService
+import org.hibernate.validator.constraints.UUID
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -17,5 +18,12 @@ class InstitutionController {
     @GetMapping("/")
     fun getAll(): ResponseEntity<List<InstitutionDto>> {
         return ResponseEntity.ok(institutionService.getAll())
+    }
+
+    @GetMapping("/{idInstitution}")
+    fun institutionItem(
+        @PathVariable @UUID idInstitution: String
+    ): ResponseEntity<InstitutionDto> {
+        return ResponseEntity.ok(institutionService.getInstitutionItem(idInstitution))
     }
 }
--- src/main/kotlin/ar/edu/unsam/pds/dto/InstitutionDto.kt
+++ src/main/kotlin/ar/edu/unsam/pds/dto/InstitutionDto.kt
@@ -2,5 +2,5 @@ package ar.edu.unsam.pds.dto
 
 import ar.edu.unsam.pds.models.Course
 
-class InstitutionDto(val id: String, val name: String, val category: String,
+class InstitutionDto(val id: String, val name: String, val description: String, val category: String,
                      val image: String, val courses: Set<Course>) {}
