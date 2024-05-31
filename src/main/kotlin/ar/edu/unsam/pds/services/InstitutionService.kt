00989a49b870779d1ec90bb08fb08cf031defdff
tventurini_meli <tomas.venturini@mercadolibre.com>
2024-05-31 19:17:17 -0300

    Add lowercase to institution search

--- src/main/kotlin/ar/edu/unsam/pds/services/InstitutionService.kt
+++ src/main/kotlin/ar/edu/unsam/pds/services/InstitutionService.kt
@@ -15,7 +15,7 @@ class InstitutionService(
 ) {
 
     fun getAll(query: String): List<InstitutionResponseDto> {
-        val institutions = institutionRepository.getAllBy(query)
+        val institutions = institutionRepository.getAllBy(query.lowercase())
         return institutions.map { Mapper.buildInstitutionDto(it) }
     }
 
