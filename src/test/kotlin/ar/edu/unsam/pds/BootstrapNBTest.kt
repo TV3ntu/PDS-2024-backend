d31d800ca788b310094bdb0354a60ea0258eae00
Rafael José <rafael_jose_calderon@hotmail.com>
2024-07-13 09:16:35 -0300

    vuelta a atras, todos los test ok

--- src/test/kotlin/ar/edu/unsam/pds/BootstrapNBTest.kt
+++ src/test/kotlin/ar/edu/unsam/pds/BootstrapNBTest.kt
@@ -7,18 +7,15 @@ import ar.edu.unsam.pds.services.StorageService
 import org.junit.jupiter.api.BeforeEach
 import org.mockito.Mock
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.mock.web.MockMultipartFile
 import org.springframework.security.web.authentication.rememberme.TokenBasedRememberMeServices
-import org.springframework.test.context.ActiveProfiles
-import org.springframework.transaction.PlatformTransactionManager
-import org.springframework.transaction.support.TransactionTemplate
+import org.springframework.session.FindByIndexNameSessionRepository
+import org.springframework.session.Session
 
-@ActiveProfiles("test")
-@SpringBootTest(classes = [ProjectApplication::class])
+@DataJpaTest
 class BootstrapNBTest : BootstrapBasicTest() {
-    @Autowired private lateinit var transactionManager: PlatformTransactionManager
-
     @Autowired lateinit var principalRepository: PrincipalRepository
     @Autowired lateinit var userRepository: UserRepository
     @Autowired lateinit var scheduleRepository: ScheduleRepository
@@ -31,20 +28,18 @@ class BootstrapNBTest : BootstrapBasicTest() {
     @Mock lateinit var emailService: EmailService
     @Mock lateinit var rememberMeServices: TokenBasedRememberMeServices
 
+    @MockBean lateinit var sessionRepository: FindByIndexNameSessionRepository<Session>
+
     lateinit var file: MockMultipartFile
 
     @BeforeEach
     fun setUpBootstrapNBTest() {
-        TransactionTemplate(transactionManager).execute {
-            users = userRepository.saveAll(users)
-            principals = principalRepository.saveAll(principals)
-            schedules = scheduleRepository.saveAll(schedules)
-            assignments = assignmentRepository.saveAll(assignments)
-            courses = courseRepository.saveAll(courses)
-            institutions = institutionRepository.saveAll(institutions)
-
-            "status"
-        }
+        userRepository.saveAll(users)
+        principalRepository.saveAll(principals)
+        scheduleRepository.saveAll(schedules)
+        assignmentRepository.saveAll(assignments)
+        courseRepository.saveAll(courses)
+        institutionRepository.saveAll(institutions)
 
         file = MockMultipartFile(
             "filename",
@@ -52,26 +47,5 @@ class BootstrapNBTest : BootstrapBasicTest() {
             "image/jpeg",
             "some content".toByteArray()
         )
-    }
-
-    fun deepEquals(self: Any?, other: Any?): Boolean {
-        if (self === other) return true
-
-        if (other !is Any) return false
-        if (self !is Any) return false
-
-        if (self.javaClass != other.javaClass) return false
-
-        val properties = this.javaClass.declaredFields
-
-        for (property in properties) {
-            property.isAccessible = true
-            val value1 = property.get(this)
-            val value2 = property.get(other)
-
-            if (value1 != value2) return false
-        }
-
-        return true
     }
 }
